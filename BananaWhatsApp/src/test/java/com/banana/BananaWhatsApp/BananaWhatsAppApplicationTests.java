ecd64e7f7af68c8ac374845be7242739e82f2c64
pablodiaz4 <pablo.diaz4@dxc.com>
2025-03-14 15:10:14 +0100

    Add files via upload
    Reto 1 curso spring-boot

---
+++ BananaWhatsApp/src/test/java/com/banana/BananaWhatsApp/BananaWhatsAppApplicationTests.java
@@ -0,0 +1,12 @@
+package com.banana.BananaWhatsApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BananaWhatsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
