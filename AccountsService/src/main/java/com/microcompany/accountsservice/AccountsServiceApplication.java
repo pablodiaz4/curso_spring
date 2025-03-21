8ae34a31cd9ce6f771ca7e7badb98188e14293f2
pablo.diaz4 <pablo.diaz4@dxc.com>
2025-03-21 12:24:29 +0100

    Añadida implementación AccountController

--- AccountsService/src/main/java/com/microcompany/accountsservice/AccountsServiceApplication.java
+++ AccountsService/src/main/java/com/microcompany/accountsservice/AccountsServiceApplication.java
@@ -1,4 +1,4 @@
-package com.microcompany.AccountsService;
+package com.microcompany.accountsservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
